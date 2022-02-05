0c07378439aad7400b71d98589b1b2e67ceb157b
Ivan Monteiro Cantalice <ivanmonteiroc@gmail.com>
2022-02-05 17:45:42 -0300

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './Home';
+import AppNavbar from "./Navbar";
+import NotFound from './NotFound';
+import MovieDetails from './MovieDetails';
+import SerieDetails from './SerieDetails';
+import SearchResults from './SearchResults';
+import ScrollToTop from './ScrollToTop';
+
+const App = () => (
+  <div className="container-fluid g-0">
+    <AppNavbar />
+    <BrowserRouter>
+      <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<Home />} />      
+            <Route path="/movies/:imdb_id" element={<MovieDetails />} />
+            <Route path="/series/:imdb_id" element={<SerieDetails />} />
+            <Route path="/search/:query" element={<SearchResults />} />
+            <Route path="*" element={<NotFound />}/>
+          </Routes>
+        </ScrollToTop>
+      </BrowserRouter>
+  </div>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
