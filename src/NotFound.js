cc5208a204dbcabdc35fdce4c5117a077dd1b1a3
Ivan Monteiro Cantalice <ivanmonteiroc@gmail.com>
2022-02-05 17:46:51 -0300

    Add not found route

---
+++ src/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export default function NotFound() {
+  return (
+    <Container>
+      <h2>Not Found</h2>
+    </Container>
+  );
+}
