c274d160b3bd19edd949685c51e1eadfde68973e
Ivan Monteiro Cantalice <ivanmonteiroc@gmail.com>
2022-02-05 20:27:54 -0300

    Improve not found page

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import TopMovies from "./movies/TopMovies";
+import TopSeries from "./series/TopSeries";
 
 export default function NotFound() {
   return (
     <Container>
-      <h2>Not Found</h2>
+      <h2>Oops...</h2>
+      <p>
+        We could not find what you're looking for.
+      </p>
+
+      <p>
+        Maybe you are looking for one of the following <Link to="/movies">movies</Link> or <Link to="/series">series</Link>:
+      </p>
+
+      <TopMovies limitResults={18}></TopMovies>
+      <TopSeries limitResults={18}></TopSeries>
     </Container>
   );
 }
